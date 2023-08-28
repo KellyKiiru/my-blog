618c7c5ab6a9ab3906a50c77eebba9b46006554c
KellyKiiru <infowithkiiru@gmail.com>
2023-08-28 12:03:38 +0300

    define blogs array; define getBlogs function that consumes from api endpoint and adds instances to blogs array

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css']
 })
-export class ArticleComponent {
+export class ArticleComponent implements OnInit{
 
+  blogs:any[]=[]
+
+  constructor(private httpClient:HttpClient) {}
+
+  getBlogs(){
+    this.httpClient.get<any[]>('http://127.0.0.1:8000/blogs/').subscribe((data:any)=>{
+      this.blogs = data;
+      console.log(data);
+    })
+  }
+
+  ngOnInit(): void {
+    this.getBlogs()
+  }
 }
