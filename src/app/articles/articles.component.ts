677966c9c237b81f44cdac54cc8dc334a47adae3
KellyKiiru <infowithkiiru@gmail.com>
2023-08-28 15:47:26 +0300

    logic to consume api

--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
 
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.css']
 })
-export class ArticlesComponent {
+export class ArticlesComponent implements OnInit {
+  blogs:any[]=[]
 
+  constructor(private httpClient:HttpClient) {}
+
+  getBlogs(){
+    this.httpClient.get<any[]>('http://127.0.0.1:8000/blogs/').subscribe((data:any[])=>{
+      this.blogs = data;
+      console.log(data);
+    })
+  }
+
+  ngOnInit(): void {
+    this.getBlogs()
+  }
 }
