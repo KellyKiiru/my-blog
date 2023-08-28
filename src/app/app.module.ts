39077d6daa174ccd3aec1190d272243d5f420a25
KellyKiiru <infowithkiiru@gmail.com>
2023-08-28 15:47:03 +0300

    import HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { AppRoutesModule } from './modules/app.routes'
+import { AppRoutesModule } from './modules/app.routes';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ArticleComponent } from './article/article.component';
@@ -16,7 +17,8 @@ import { ArticlesComponent } from './articles/articles.component';
   imports: [
     BrowserModule,
     AppRoutesModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
