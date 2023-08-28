6329ad76f7c553078c001a4be8ed05db30baed61
KellyKiiru <infowithkiiru@gmail.com>
2023-08-28 12:07:27 +0300

    add route to blogs page

--- src/app/modules/app.routes.ts
+++ src/app/modules/app.routes.ts
@@ -2,9 +2,11 @@ import { NgModule } from "@angular/core";
 import { RouterModule,Routes } from "@angular/router";
 
 import { ArticleComponent } from "../article/article.component";
+import { ArticlesComponent } from "../articles/articles.component";
 
 const routes:Routes = [
     {path:'', component:ArticleComponent},
+    {path:'/blogs', component:ArticlesComponent}
 ]
 
 @NgModule({
